4fa4826455531d6a36fae9afc114f64dd882c4df
Tarasov Aleksey <leo.TosTaroVi4@gmail.com>
2026-03-16 02:18:10 +0300

    Domashka 01

--- src/Domashka01/Domashka1_1.java
+++ src/Domashka01/Domashka1_1.java
@@ -1,4 +1,15 @@
 package Domashka01;
 
 public class Domashka1_1 {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b = 7;
+        int c = 5;
+
+        a = 4 * (b + c) / 2;
+
+        System.out.println(a);
+    }
 }
---
+++ src/Domashka01/Domashka1_6.java
@@ -0,0 +1,18 @@
+package Domashka01;
+
+public class Domashka1_6 {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+        int c;
+        System.out.println(a + " " + b);
+
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println(a + " " + b);
+    }
+}
